cb1831334a0fb98bb4e44c7f92bda07f95cd10a7
Michael O'Shea <michael.a.oshea@gmail.com>
2024-12-05 16:30:37 +0000

    adjusted the welcom message on the index.html page

--- learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aef2593f309edbe932f4a7dbd9a157f4cf303db")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
